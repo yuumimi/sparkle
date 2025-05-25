f6e2eea81341c5fa6cc8b3b92654d423da06cb82
xishang0128 <xishang02@gmail.com>
2025-05-25 16:09:24 +0800

    correct defaultBypass assignment

--- src/main/sys/sysproxy.ts
+++ src/main/sys/sysproxy.ts
@@ -10,44 +10,6 @@ let defaultBypass: string[]
 let triggerSysProxyTimer: NodeJS.Timeout | null = null
 const helperSocketPath = '/tmp/mihomo-party-helper.sock'
 
-if (process.platform === 'linux')
-  defaultBypass = ['localhost', '127.0.0.1', '192.168.0.0/16', '10.0.0.0/8', '172.16.0.0/12', '::1']
-if (process.platform === 'darwin')
-  defaultBypass = [
-    '127.0.0.1',
-    '192.168.0.0/16',
-    '10.0.0.0/8',
-    '172.16.0.0/12',
-    'localhost',
-    '*.local',
-    '*.crashlytics.com',
-    '<local>'
-  ]
-if (process.platform === 'win32')
-  defaultBypass = [
-    'localhost',
-    '127.*',
-    '192.168.*',
-    '10.*',
-    '172.16.*',
-    '172.17.*',
-    '172.18.*',
-    '172.19.*',
-    '172.20.*',
-    '172.21.*',
-    '172.22.*',
-    '172.23.*',
-    '172.24.*',
-    '172.25.*',
-    '172.26.*',
-    '172.27.*',
-    '172.28.*',
-    '172.29.*',
-    '172.30.*',
-    '172.31.*',
-    '<local>'
-  ]
-
 export async function triggerSysProxy(enable: boolean): Promise<void> {
   if (net.isOnline()) {
     if (enable) {
@@ -63,6 +25,51 @@ export async function triggerSysProxy(enable: boolean): Promise<void> {
 }
 
 async function enableSysProxy(): Promise<void> {
+  if (process.platform === 'linux')
+    defaultBypass = [
+      'localhost',
+      '.local',
+      '127.0.0.1/8',
+      '192.168.0.0/16',
+      '10.0.0.0/8',
+      '172.16.0.0/12',
+      '::1'
+    ]
+  if (process.platform === 'darwin')
+    defaultBypass = [
+      '127.0.0.1/8',
+      '192.168.0.0/16',
+      '10.0.0.0/8',
+      '172.16.0.0/12',
+      'localhost',
+      '*.local',
+      '*.crashlytics.com',
+      '<local>'
+    ]
+  if (process.platform === 'win32')
+    defaultBypass = [
+      'localhost',
+      '127.*',
+      '192.168.*',
+      '10.*',
+      '172.16.*',
+      '172.17.*',
+      '172.18.*',
+      '172.19.*',
+      '172.20.*',
+      '172.21.*',
+      '172.22.*',
+      '172.23.*',
+      '172.24.*',
+      '172.25.*',
+      '172.26.*',
+      '172.27.*',
+      '172.28.*',
+      '172.29.*',
+      '172.30.*',
+      '172.31.*',
+      '<local>'
+    ]
   await startPacServer()
   const { sysProxy } = await getAppConfig()
   const { mode, host, bypass = defaultBypass } = sysProxy
