6d01974ba7afffe89713f94ce0fbac5934f8702f
xishang0128 <xishang02@gmail.com>
2025-05-26 17:42:53 +0800

    supplement missing clearTimeout

--- src/main/core/profileUpdater.ts
+++ src/main/core/profileUpdater.ts
@@ -63,6 +63,7 @@ export async function addProfileUpdater(item: IProfileItem): Promise<void> {
 
 export async function delProfileUpdater(id: string): Promise<void> {
   if (intervalPool[id]) {
+    clearTimeout(intervalPool[id])
     delete intervalPool[id]
   }
 }
