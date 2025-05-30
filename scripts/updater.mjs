aa2b3dc44658b910e7d988c0c59e515578c13369
xishang0128 <xishang02@gmail.com>
2025-05-31 03:22:47 +0800

    adjust updater

--- scripts/updater.mjs
+++ scripts/updater.mjs
@@ -10,13 +10,14 @@ const latest = {
   changelog
 }
 
-changelog += '\n### 下载地址：\n\n#### Windows10/11：\n\n'
-changelog += `- 安装版：[64位](${downloadUrl}/sparkle-windows-${version}-x64-setup.exe) | [ARM64](${downloadUrl}/sparkle-windows-${version}-arm64-setup.exe)\n\n`
-changelog += '\n#### macOS 11+：\n\n'
-changelog += `- PKG：[Intel](${downloadUrl}/sparkle-macos-${version}-x64.pkg) | [Apple Silicon](${downloadUrl}/sparkle-macos-${version}-arm64.pkg)\n\n`
-changelog += '\n#### Linux：\n\n'
-changelog += `- DEB：[64位](${downloadUrl}/sparkle-linux-${version}-amd64.deb) | [ARM64](${downloadUrl}/sparkle-linux-${version}-arm64.deb)\n\n`
-changelog += `- RPM：[64位](${downloadUrl}/sparkle-linux-${version}-x86_64.rpm) | [ARM64](${downloadUrl}/sparkle-linux-${version}-aarch64.rpm)`
-
+if (!version.includes('beta')) {
+  changelog += '\n### 下载地址：\n\n#### Windows10/11：\n\n'
+  changelog += `- 安装版：[64位](${downloadUrl}/sparkle-windows-${version}-x64-setup.exe) | [ARM64](${downloadUrl}/sparkle-windows-${version}-arm64-setup.exe)\n\n`
+  changelog += '\n#### macOS 11+：\n\n'
+  changelog += `- PKG：[Intel](${downloadUrl}/sparkle-macos-${version}-x64.pkg) | [Apple Silicon](${downloadUrl}/sparkle-macos-${version}-arm64.pkg)\n\n`
+  changelog += '\n#### Linux：\n\n'
+  changelog += `- DEB：[64位](${downloadUrl}/sparkle-linux-${version}-amd64.deb) | [ARM64](${downloadUrl}/sparkle-linux-${version}-arm64.deb)\n\n`
+  changelog += `- RPM：[64位](${downloadUrl}/sparkle-linux-${version}-x86_64.rpm) | [ARM64](${downloadUrl}/sparkle-linux-${version}-aarch64.rpm)`
+}
 writeFileSync('latest.yml', yaml.stringify(latest))
 writeFileSync('changelog.md', changelog)
