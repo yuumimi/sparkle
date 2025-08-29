0b38f8675709af1a1280594ed618290c125988c9
xishang0128 <xishang02@gmail.com>
2025-08-29 12:13:02 +0800

    fix yaml parsing

--- src/main/utils/yaml.ts
+++ src/main/utils/yaml.ts
@@ -1,9 +1,13 @@
 import yaml from 'yaml'
 
 export function parseYaml<T = unknown>(content: string): T {
-  const processedContent = content.replace(/(short-id:\s*)([^"\s\n]+)/g, (match, prefix, value) => {
-    return value === 'null' ? match : `${prefix}"${value}"`
-  })
+  const processedContent = content.replace(
+    /^(\s*short-id:\s*)([^"\s\n}{]+)/gm,
+    (match, prefix, value) => {
+      const nullValues = ['null', 'Null', 'NULL', '~']
+      return nullValues.includes(value) ? match : `${prefix}"${value}"`
+    }
+  )
 
   const result =
     yaml.parse(processedContent, {
