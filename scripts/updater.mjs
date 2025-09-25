dd6a3102708dfa217c7a4d6999c474e24b7c60f7
miwu04 <me@alanlin.icu>
2025-09-25 17:55:38 +0800

    update changelog generation

--- scripts/updater.mjs
+++ scripts/updater.mjs
@@ -16,8 +16,8 @@ if (process.env.SKIP_CHANGELOG !== '1') {
   changelog += '\n#### macOS 11+：\n\n'
   changelog += `- PKG：[Intel](${downloadUrl}/sparkle-macos-${version}-x64.pkg) | [Apple Silicon](${downloadUrl}/sparkle-macos-${version}-arm64.pkg)\n\n`
   changelog += '\n#### Linux：\n\n'
-  changelog += `- DEB：[64位](${downloadUrl}/sparkle-linux-${version}-amd64.deb) | [ARM64](${downloadUrl}/sparkle-linux-${version}-arm64.deb) | [loong64](${downloadUrl}/sparkle-linux-${version}-loong64.deb)\n\n`
-  changelog += `- RPM：[64位](${downloadUrl}/sparkle-linux-${version}-x86_64.rpm) | [ARM64](${downloadUrl}/sparkle-linux-${version}-aarch64.rpm) | [loong64](${downloadUrl}/sparkle-linux-${version}-loong64.rpm)\n\n`
+  changelog += `- DEB：[64位](${downloadUrl}/sparkle-linux-${version}-amd64.deb) | [ARM64](${downloadUrl}/sparkle-linux-${version}-arm64.deb) | [loong64](${downloadUrl}/sparkle-linux-${version}-loong64.deb) | [loong64(aosc-compatible)](${downloadUrl}/sparkle-linux-${version}-loongarch64.deb)\n\n`
+  changelog += `- RPM：[64位](${downloadUrl}/sparkle-linux-${version}-x86_64.rpm) | [ARM64](${downloadUrl}/sparkle-linux-${version}-aarch64.rpm) | [loong64](${downloadUrl}/sparkle-linux-${version}-loongarch64.rpm)\n\n`
   changelog += `- PACMAN：[64位](${downloadUrl}/sparkle-linux-${version}-x64.pkg.tar.xz) | [ARM64](${downloadUrl}/sparkle-linux-${version}-aarch64.pkg.tar.xz) | [loong64](${downloadUrl}/sparkle-linux-${version}-loong64.pkg.tar.xz)`
 }
 writeFileSync('latest.yml', yaml.stringify(latest))
